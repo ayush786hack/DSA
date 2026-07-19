fcb53d8ac7ca2c41160b80352b995a823480063d
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-07-19 16:41:36 +0530

    Time: 0 ms (100%), Space: 43.8 MB (46.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,33 @@
 class Solution {
       public int search(int[] nums, int target) {
-
-        // Initialize search space
-        int low = 0;
-        int high = nums.length - 1;
-
-        // Continue while there is still a valid search range
-        while (low <= high) {
-
-            // Calculate middle index
-            int mid = (low + high) / 2;
-
-            // If target found, return index
-            if (nums[mid] == target)
+        int n=nums.length;
+           int low=0;
+           int high=n-1;
+          
+          while(low<=high){
+              int mid =(low+high)/2;
+        
+                  if(nums[mid]==target){
                 return mid;
-
-            // If left part is sorted
-            if (nums[low] <= nums[mid]) {
-
-                // If target lies within sorted left part
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
-                }
-                // Else, search in right half
-                else {
-                    low = mid + 1;
-                }
+              }
+          
+         if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target<nums[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
-
-            // Else, right part is sorted
-            else {
-
-                // If target lies within sorted right part
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
-                }
-                // Else, search in left half
-                else {
-                    high = mid - 1;
-                }
+           }
+         else{
+            if(nums[mid]<target && target<=nums[high]){
+                low=mid+1;
             }
-        }
+            else{
+                high=mid-1;
+            }
+         }
 
-        // Target not found
-        return -1;
-    }
+          }
+         return -1;       
+  }
 }
