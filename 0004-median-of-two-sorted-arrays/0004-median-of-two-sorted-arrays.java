cc633fc77e45e4ddfd5472da405562a03c48b3f4
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-07-20 23:50:51 +0530

    Time: 8 ms (14.37%), Space: 49 MB (16.01%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,43 +1,23 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        // Make sure nums1 is the smaller array
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
+        int arr[]=new int [nums1.length+nums2.length];
+
+        for(int i=0;i<nums1.length;i++){
+            arr[i]=nums1[i];
+        }
+     for (int j = 0; j < nums2.length; j++) {
+    arr[nums1.length + j] = nums2[j];
+}
+        Arrays.sort(arr);
+        int low=0;
+        int high=arr.length-1;
+        int mid=(low+high)/2;
+        if(arr.length%2==1){
+            return arr[mid];
+        }else{
+            
+return (arr[mid]+arr[mid+1])/2.0;
         }
 
-        int m = nums1.length;
-        int n = nums2.length;
-
-        int low = 0;
-        int high = m;
-
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int left1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int right1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-
-            int left2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            int right2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (left1 <= right2 && left2 <= right1) {
-
-                // Even number of elements
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(left1, left2) + Math.min(right1, right2)) / 2.0;
-                }
-
-                // Odd number of elements
-                return Math.max(left1, left2);
-
-            } else if (left1 > right2) {
-                high = cut1 - 1;
-            } else {
-                low = cut1 + 1;
-            }
-        }
-
-        return 0.0;
     }
 }
