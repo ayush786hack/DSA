6f02aaa81ed6180ead325c71bf1ec9ebc050ec1a
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-07-30 15:21:26 +0530

    Time: 264 ms (41.68%), Space: 43.7 MB (56.52%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int repeatedStringMatch(String a, String b) {
+      StringBuilder sb =new StringBuilder();
+      int count=0;
+      while(sb.length()<b.length()+a.length()){
+          sb.append(a);
+          count++;
+             if (sb.indexOf(b) != -1){
+                return count;
+        }
+      }
+
+         return -1;
+      } 
+      
+    }
