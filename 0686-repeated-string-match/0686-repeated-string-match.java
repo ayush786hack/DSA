ea04e130673c7d27f5ac1d6635650be6a96f32ec
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-07-30 15:38:49 +0530

    Time: 261 ms (63.66%), Space: 43.6 MB (70.68%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -2,7 +2,7 @@ class Solution {
     public int repeatedStringMatch(String a, String b) {
       StringBuilder sb =new StringBuilder();
       int count=0;
-      while(sb.length()<b.length()+a.length()){
+      while(sb.length()<=b.length()+a.length()){
           sb.append(a);
           count++;
              if (sb.indexOf(b) != -1){
