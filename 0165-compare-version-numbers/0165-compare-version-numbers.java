eced7916eb5be01fd60f956c73132535c7ff55be
Ayush tiwari <ayushtiwari92205@gmail.com>
2026-08-02 19:30:05 +0530

    Time: 0 ms (100%), Space: 42.3 MB (98.73%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,19 +1,34 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-       String[] v1 = version1.split("\\.");
-       String[] v2 = version2.split("\\."); 
-       
-       for(int i=0;i<Math.max(v1.length,v2.length);i++){
-        int num1 = (i < v1.length) ? Integer.parseInt(v1[i]) : 0;
-int num2 = (i < v2.length) ? Integer.parseInt(v2[i]) : 0;
-       if( num1>num2){
-        return 1;
-       }
-
-         if(num1<num2){
-        return -1;
-       }
-       }
-       return 0;
-    }
+   int i=0;
+   int j=0;
+   
+     while(i<version1.length() || j<version2.length()){
+        int num1=0;
+   int num2=0;
+  while (i < version1.length() && version1.charAt(i) != '.') {
+    num1=num1*10+version1.charAt(i)-'0';
+    i++;
 }
+  while (j< version2.length() && version2.charAt(j) != '.') {
+    num2=num2*10+version2.charAt(j)-'0';
+    j++;
+}
+if(i<version1.length()&& version1.charAt(i)=='.'){
+    i++;
+}
+if(j<version2.length()&& version2.charAt(j)=='.'){
+    j++;
+}
+if(num1<num2){
+    return -1;
+
+}
+if(num1>num2){
+    return 1;
+
+}
+     }
+      return 0;  
+    }
+ }
